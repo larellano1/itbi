0a30c29f6b8ccae047ad9d5e55007f1b273971da
Arellano <lfv.arellano@gmail.com>
2022-07-28 14:15:02 -0300

    Add files via upload

--- plot_primer.py
+++ plot_primer.py
@@ -5,13 +5,13 @@ df = carrega_dados_uso()
 fig = px.scatter(df, x='Área Construída (m2)', y='Valor de Transação (declarado pelo contribuinte)', color='ACC (IPTU)')
 map = cria_mapa(df)
 map.save('mapa.html')
-
+print(df.to_dict())
 
 app = Dash(__name__)
 
 
 
-app.layout = html.Div([
+app.layout = html.Div(children=[
     html.H1("Quanto vale o meu apê?"),
     html.Div(children=[
     html.Div(children=[
@@ -24,15 +24,15 @@ app.layout = html.Div([
             html.H2(f"Análise das transações:"),
             dcc.Graph(figure=fig),
             html.H2("Transações na região:"),
-            dash_table.DataTable(df.to_dict('records'), 
-                            columns=[{'name': i, 'id': j, 'type':'numeric', 'format':dash_table.FormatTemplate.money(2)} if i == 'Valor Declarado' 
-                                                                                                                        else {'name': i, 'id': j} for i, j in zip(['Rua','Número','Complemento','CEP','Data','Terreno(m2)','Apartamento (m2)','Ano de Construção','Valor Declarado'],
-                                                                                                                                                                    ['Nome do Logradouro','Número','Complemento','CEP', 'Data de Transação','Área do Terreno (m2)','Área Construída (m2)', 'ACC (IPTU)', 'Valor de Transação (declarado pelo contribuinte)'])],
-                            filter_action="native",
-                            sort_action="native",
-                            sort_mode="multi",),
+            dash_table.DataTable(df.drop(columns=['endereco_completo','geometry']).to_dict('records'), 
+                             columns=[{'name': i, 'id': j, 'type':'numeric', 'format':dash_table.FormatTemplate.money(2)} if i == 'Valor Declarado' 
+                                                                                                                         else {'name': i, 'id': j} for i, j in zip(['Rua','Número','Complemento','CEP','Data','Terreno(m2)','Apartamento (m2)','Ano de Construção','Valor Declarado'],
+                                                                                                                                                                     ['Nome do Logradouro','Número','Complemento','CEP', 'Data de Transação','Área do Terreno (m2)','Área Construída (m2)', 'ACC (IPTU)', 'Valor de Transação (declarado pelo contribuinte)'])],
+                             filter_action="native",
+                             sort_action="native",
+                             sort_mode="multi",),
             html.H2("Mapa das transações:"),
-            html.Iframe(id='map', srcDoc = open('mapa.html').read(), width='100%', height='600')
+            html.Iframe(id='map', srcDoc = open('mapa.html').read(), width='100%', height='600'),
         ], id='output-bairro')
     ],
     style={'padding': '30px'}),
